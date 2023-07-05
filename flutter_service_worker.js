eea0fd9bc2f588899609ec962328e139d571f194
pkamit <amit.pandey812@gmail.com>
2023-07-05 16:28:07 +0530

    category images

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {"assets/AssetManifest.json": "f8382a3b2c556364f6c95b407cfe70c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "db7363668cbd458c3274fc65f0e6902b",
-"/": "db7363668cbd458c3274fc65f0e6902b",
+"index.html": "aad1c88895302f478f02654e383e7713",
+"/": "aad1c88895302f478f02654e383e7713",
 "main.dart.js": "1b86cace857951ddfcf1d08d655ce1fb",
 "manifest.json": "f7d10cd0048ba208bbc615dbad1d295e",
 "version.json": "9c98b752100e91b3997666b294c17d16"};
