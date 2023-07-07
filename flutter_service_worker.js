f1265f956322067faeaf1edb39589133703395f5
pkamit <amit.pandey812@gmail.com>
2023-07-07 12:26:08 +0530

    product listing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {"assets/AssetManifest.json": "8e6928df9c6d2226b546f552fcdce29
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "327b8a1ecd21fd5059ba9c08e327a3b1",
-"/": "327b8a1ecd21fd5059ba9c08e327a3b1",
-"main.dart.js": "8857e6ea4cfbb386110da49143da8c0e",
+"index.html": "55fb77c459fc4d8ddea98f1f51309a05",
+"/": "55fb77c459fc4d8ddea98f1f51309a05",
+"main.dart.js": "93727c6ecf9b383617fa47debe9ab2c4",
 "manifest.json": "f7d10cd0048ba208bbc615dbad1d295e",
 "version.json": "9c98b752100e91b3997666b294c17d16"};
 // The application shell files that are downloaded before a service worker can
